1f096cdb7c39888916a8e6fcc6a28078243b4803
vikas <vikasjakkula08@gmail.com>
2025-07-09 10:03:50 +0530

    // mistake updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -84,7 +84,7 @@ function AnimatedTestimonialsDemo() {
 function AnimatedPinDemo() {
   return (
     <div className="h-[40rem] w-full flex items-center justify-center ">
-      <PinContainer title="x.com/vikas_070v" href="https:/x.com/vikas_070v">
+      <PinContainer title="x.com/vikas_070v" href="https://x.com/vikas_070v">
         <div
           className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
